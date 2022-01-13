a4c82d5f339f2253a2e262c8d1461d62a0189fb9
browonkim <browonkim@gmail.com>
2022-01-14 02:43:19 +0900

    add test-server (express)

--- fetch-example/server.js
+++ fetch-example/server.js
@@ -5,7 +5,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, 'index.html'), (error) => {
+    res.sendFile(__dirname+'/index.html', (error) => {
         if (typeof error !== 'undefined') {
             console.log(error.message);
         }
